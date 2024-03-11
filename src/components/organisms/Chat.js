984c10e89cb7129ce51d26dd28da209a33e8bf65
oktaviardi pratama <oktaviardi2510@gmail.com>
2024-03-11 10:25:45 +0700

    update

--- src/components/organisms/Chat.js
+++ src/components/organisms/Chat.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { faArrowRight, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { faPaperclip, faFaceSmile } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react';
 import Image from 'next/image';
@@ -32,9 +32,9 @@ const DashboardHeader = () => {
   return (
     <>
       <div className="flex items-center justify-between mb-2">
-        <h3 className='font-semibold'>Analytic</h3>
+        <h3 className='font-semibold'>Eshter Howard</h3>
         
-        <div className="relative inline-block">
+        {/* <div className="relative inline-block">
 
           <FontAwesomeIcon icon={faEllipsisVertical} className='w-[4px] inline' onClick={toggleDropdown}/>
             {isOpen && (
@@ -47,6 +47,39 @@ const DashboardHeader = () => {
                 </div>
               </div>
             )}
+        </div> */}
+      </div>
+      <div className='conversation-wrap'>
+        <div className='overflow-y-scroll min-h-3 max-h-[300px]'>
+          
+          <div className='content-left flex items-center w-full justify-end  col-span- gap-4 py-[10px]'>
+            <span className='block text-[16px] font-medium p-[10px] bg-blue-600 text-white rounded-lg rounded-br-none'>Are you ready?</span>
+            <div className='w-[60px]'>
+              <Image src='/man.png' className='w-[80% rounded-full' alt="My Image" width={60} height={60} />
+            </div>
+          </div>
+
+          <div className='content-left flex items-center w-full justify-start col-span- gap-4 py-[10px]'>
+            <div className='w-[60px]'>
+              <Image src='/man.png' className='w-[80% rounded-full' alt="My Image" width={60} height={60} />
+            </div>
+            <span className='block text-[16px] font-medium p-[10px] bg-gray-100 rounded-lg rounded-bl-none'> I have Prepared everything</span>
+          </div>
+
+        </div>
+        <div className='inputForm bg-gray-100 w-full min-h-[50px] rounded-md p-[15px]'>
+          <div>
+            <textarea row="3" placeholder='Type Your Message' className='bg-transparent shadow-none w-full outline-none resize-none'></textarea>
+          </div>
+          <div className='flex w-full items-center justify-between'>
+            <div className='flex items-center'>
+              <FontAwesomeIcon className='text-gray-700 p-2 text-[18px] hover:cursor-pointer' icon={faFaceSmile} />
+              <FontAwesomeIcon className='text-gray-700 p-2 text-[18px] hover:cursor-pointer' icon={faPaperclip} />
+            </div>
+            <div>
+              <button className='block w-full text-center text-[14px] text-white bg-[#5363EF] py-3 rounded-lg px-[15px]'>Send Now</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
